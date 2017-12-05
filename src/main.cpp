6c4dad6919f5113a1a547dbc32f2fba30cd4a092
Dmitriy Vornychev <dm.s.vornychev@gmail.com>
2017-12-05 06:14:34 +0300

    Улучшенный вариант на синглтонах.

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,32 @@
 
 using namespace TrickyLogger;
 
+// вывод в cout
+TLOG_MAIN_COUT()
+
+// или в указанный файл - раскомментировать одно, закомментировать другое
+//TLOG_MAIN(BufferedFileLogger::Init("debug.log"))
+
 int main(int argc, char** argv)
 {
-	//auto logger = new BufferedStreamLogger(&std::cout);
-	auto logger = new BufferedFileLogger("debug.log");
+	TLOG( "Запись в лог 1/6", MessagePriority::Info);
 
-	TLOG(logger, "Запись в лог 1/5", TrickyLogger::MessagePriority::Info)
+	// при желании, можно переопределить цель вывода...
+	//TLOG_INIT(BufferedFileLogger::Init("debug2.log"))
 
-	logger->Flush();
+	TLOG( "Запись в лог 2/6", Debug);	// не должна выводиться - приоритет Debug ниже, чем приоритет лога по умолчанию
+	TLOG( "Запись в лог 3/6", Error);
+	
+	// ...изменить приоритет...
+	//TLOG_PRIOR(Error) // debug, info и warning игнорируются
 
+	TLOG( "Запись в лог 4/6", Fatal);
+	TLOG( "Запись в лог 5/6", Info);
+	
+	// ...вручную управлять сбросом буфера, если используется буферизация
+	//TLOG_FLUSH()
 
-	TLOG(logger, "Запись в лог 2/5", TrickyLogger::MessagePriority::Debug)
-	TLOG(logger, "Запись в лог 3/5", TrickyLogger::MessagePriority::Error)
-	TLOG(logger, "Запись в лог 4/5", TrickyLogger::MessagePriority::Fatal)
-	TLOG(logger, "Запись в лог 5/5", TrickyLogger::MessagePriority::Info)
-
-	logger->Flush();
-
-
-	delete logger;
+	TLOG( "Запись в лог 6/6", Info);
 
 	return 0;
 }
--- src/message_logger.h
+++ src/message_logger.h
@@ -17,13 +17,36 @@
 namespace TrickyLogger
 {
 
-#define TLOG(logger, message, prior) \
-(logger)->Write(TrickyLogger::DetailedMessage((message),(prior), __FILE__,__LINE__));
+// объявления перед main - нужны для определения статической переменной Log
+// стандартный вывод в cout
+#define TLOG_MAIN_COUT() \
+ILogger* TrickyLogger::Log::current = StreamLogger::Init(&std::cout);
 
+// подставить сюда объект любого класса логгера
+#define TLOG_MAIN(logger) \
+ILogger* TrickyLogger::Log::current = (logger);
 
-	// TODO: глянь https://habrahabr.ru/post/225363/
+// функции ... макросы по управлению логом
+// инициализация лога 
+#define TLOG_INIT(logger) \
+TrickyLogger::Log::Init((logger));
+
+// установка приоритета
+#define TLOG_PRIOR(prior) \
+TrickyLogger::Log::SetPriority((prior));
+
+// запись в лог с указанным приоритетом
+#define TLOG(message, prior) \
+TrickyLogger::Log::Write(TrickyLogger::DetailedMessage((message),(prior), __FILE__,__LINE__));
+
+// оборачиваем Flush(), чтобы ко всем функциям можно было обращаться одинаковыми способами
+#define TLOG_FLUSH() \
+TrickyLogger::Log::Flush();
+
+	// Источники вдохновения:
 	// stack tracer https://habrahabr.ru/post/302170/
-	// easy logging https://github.com/muflihun/easyloggingpp/tree/master/src
+	// easy logging https://habrahabr.ru/post/225363/
+	// https://github.com/muflihun/easyloggingpp/tree/master/src
 
 	// возможные приоритеты сообщений
 	enum MessagePriority
@@ -48,25 +71,13 @@ namespace TrickyLogger
 		case Warning:
 			return "Warning";
 		case Error:
-			return "Error";
+			return "Error!";
 		case Fatal:
-			return "FATAL ERROR";
+			return "FATAL";
 		default: 
 			return "Unknown message type";
 		}
 	}
-
-	// std::string timeStr(time_t time)
-	// {
-	// 	std::string result;
-
-	// 	return resultstd::string timeStr(time_t time)
-	// {
-	// 	std::string result;
-
-	// 	return result;
-	;
-	// }
 
 	// интерфейс, определяющий базовые требования к сообщениям
 	class IMessage
@@ -90,11 +101,13 @@ namespace TrickyLogger
 		MessagePriority getPriority() { return _priority; }	// не подразумеваем изменения типа сообщения после создания
 	};
 
-	// сообщение с дополнительной информацией
+	// сообщение с дополнительной информацией.
+	// После того, как решили вынести создание строки непосредственно в логгер, теряется смысл в выделении просто IMessage:
+	// - все сообщения в актуальных реалиациях ILogger объявлены Detailed
 	class DetailedMessage : public IMessage
 	{
 		time_t _t;	// https://stackoverflow.com/questions/997946/how-to-get-current-time-and-date-in-cs
-		std::string _file;	// я так понимаю, в задании имеется в виду место, откуда пишется сообщение. Оставим просто строку
+		std::string _file;
 		int _lineNumber;
 
 	public:
@@ -113,52 +126,93 @@ namespace TrickyLogger
 		int getLine() { return _lineNumber; }
 	};
 
-	// собственно, логгер.
-	// На данный момент в виде интерфейса, потому что подразумевается множество различных реализаций 
+	// собственно, логгер
 	class ILogger
 	{
 	protected:
 		MessagePriority _priority;	// разрешённый приоритет сообщений - с более низким приоритетом игнорируются
-		//std::vector<IMessage> list; // исключили из общего интерфейса, чтобы допустить реализации,
-			// которые пишут напрямую в цель
+		
+		// подклассы будут построены по принципу синглтона, так что прячем.
+		ILogger(MessagePriority prior = MessagePriority::Info) : _priority(prior) {}
+		virtual ~ILogger() {}	
 
 	public:
-		ILogger() : _priority(MessagePriority::Info) {}
-		virtual ~ILogger() {}
 		
 		virtual void Write(DetailedMessage message) = 0; // добавить сообщение
-		virtual int Flush() = 0;	// запись результатов логгирования в искомый поток... искомую цель.
-			// - запускать в отдельном потоке?
+		virtual int Flush() { return 0; };	// запись результатов логгирования в искомый поток... искомую цель.
+			// Вообще говоря, метод нужен только объектам с буферизацией в том или ином виде. Но поскольку их
+			// довольно-таки много, а мы хотим унифицировать доступ к подклассам, включаем метод в базовый интерфейс
 
 		MessagePriority GetPriority() {return _priority; } // возвращает текущий уровень приоритета.
 		virtual int SetPriority(MessagePriority newPriority) // изменяет уровень приоритета.
 		{ 
 			_priority = newPriority;
 			return 0;	// согласно текущей идее, возвращать мы должны количество сообщений,
-				// не подходящих по установленному приоритету, но уже находящихся в списке
+				// не подходящих по установленному приоритету, но уже находящихся в буфере
 		}
-			// виртуальный, потому что мы можем захотеть изменить поведение, 
+			// метод SetPriority виртуальный, потому что мы можем захотеть изменить поведение, 
 			// например, удалять из буфера сообщения с более низким приоритетом
 
-		//void Format(std::string format);	// установить "вид" сообщения для вывода
-
-		//int Check();	// проверяет все сообщения на соответствие заданному приоритету и удаляет лишние - нужна ли? 
+		//void Format(std::string format);	// ? установить "вид" сообщения для вывода
 	};
 
 	// берём интерфейс и дополняем его буфером сообщений и связью с "реальностью"
 	// добавить промежуточный класс BufferedLogger ?
-	class BufferedStreamLogger : public ILogger
+	class StreamLogger : public ILogger
 	{
-		std::list<DetailedMessage> _list;	// IMessage
+	protected:
 		std::ostream* _target;
-	public:
-		BufferedStreamLogger(std::ostream* target) : _target(target) {}
 
-		~BufferedStreamLogger()
+		StreamLogger(std::ostream* target) : _target(target) {}
+		~StreamLogger() {}
+
+	public:
+
+		static StreamLogger* Init(std::ostream* target)
 		{
-			Flush();	// перед завершением работы скидываем в лог всё, что накопилось
+			static StreamLogger logger(target);
+			// если хотим инициализировать другим потоком
+			if (target != logger._target) logger._target = target;
+
+			return &logger;
 		}
 
+		// "сбрасываем" буфер, записывая его содержимое в целевой поток
+		void Write(DetailedMessage message) override
+		{	
+			// если так получилось, что в список затесались сообщения с более низким приоритетом,
+			// чем нам хотелось бы - пропускаем
+			if (message.getPriority() < this->_priority) return;
+
+			auto curTime = message.getTime();
+			auto ltm = localtime(&curTime);
+
+			// вывод времени в "человеческом" виде
+			*_target << ltm->tm_year + 1900 << "." 
+						<< ltm->tm_mon + 1 << "."
+						<< ltm->tm_mday << " "
+						<< ltm->tm_hour + 1 << ":"
+						<< ltm->tm_min + 1 << ":"
+						<< ltm->tm_sec + 1 << "\t";
+
+			// если мы определяем буффер как список IMessage, то мы не получим доступа к полям DetailedMessage,
+			// и таким образом должны определять формат вывода в рамках подклассов IMessage
+			*_target << PriorityToString(message.getPriority()) << '\t'
+						<< message.getBody() << '\t'
+						<< message.getFile() << '\t'						 
+						<< message.getLine() << std::endl;
+		}
+	};
+
+	class IBufferedLogger : public ILogger
+	{
+	protected:
+		std::list<DetailedMessage> _list;	// IMessage
+
+		virtual void WriteMessage(DetailedMessage message) = 0; 
+			// собственно, запись сообщения куда бы то ни было
+
+	public:
 		void Write(DetailedMessage message) override
 		{
 			// разрешаем запись сообщений только с равным или более высоким приоритетом
@@ -168,48 +222,86 @@ namespace TrickyLogger
 			}
 		}
 
-
-		// "сбрасываем" буфер, записывая его содержимое в целевой поток
 		int Flush() override
 		{	
 			for (auto message : _list)
-			{
-				auto curTime = message.getTime();
-				auto ltm = localtime(&curTime);
-
-				// вывод времени в "человеческом" виде
-				*_target << ltm->tm_year + 1900 << "." 
-						 << ltm->tm_mon + 1 << "."
-						 << ltm->tm_mday << " "
-						 << ltm->tm_hour + 1 << ":"
-						 << ltm->tm_min + 1 << ":"
-						 << ltm->tm_sec + 1 << "\t";
-
-				// если мы определяем буффер как список IMessage, то мы не получим доступа к полям DetailedMessage,
-				// и таким образом должны определять формат вывода в рамках подклассов IMessage
-				*_target << PriorityToString(message.getPriority()) << '\t'
-						 << message.getBody() << '\t'
-						 << message.getFile() << '\t'						 
-						 << message.getLine() << std::endl;
-
-				// кастомизация формата вывода за счёт настроек класса?
+			{				
+				WriteMessage(message);
 			}
 
 			_list.clear();	// очищаем список
 
 			return 0;	// возвращать количество записанных сообщений?
 		}
+
+	};
+
+	// берём интерфейс и дополняем его буфером сообщений и связью с "реальностью"
+	// добавить промежуточный класс BufferedLogger ?
+	class BufferedStreamLogger : public IBufferedLogger
+	{
+	protected:
+		std::ostream* _target;
+
+		BufferedStreamLogger(std::ostream* target) : _target(target) {}
+		~BufferedStreamLogger()
+		{
+			Flush();	// перед завершением работы скидываем в лог всё, что накопилось
+		}
+
+		// "сбрасываем" буфер, записывая его содержимое в целевой поток
+		void WriteMessage(DetailedMessage message) override
+		{	
+			// если так получилось, что в список затесались сообщения с более низким приоритетом,
+			// чем нам хотелось бы - пропускаем
+			if (message.getPriority() < this->_priority) return;
+
+			auto curTime = message.getTime();
+			auto ltm = localtime(&curTime);
+
+			// вывод времени в "человеческом" виде
+			*_target << ltm->tm_year + 1900 << "."; 
+			_target->width(2); _target->fill('0'); *_target << ltm->tm_mon + 1 << ".";
+			_target->width(2); _target->fill('0'); *_target << ltm->tm_mday << " ";
+			_target->width(2); _target->fill('0'); *_target << ltm->tm_hour + 1 << ":";
+			_target->width(2); _target->fill('0'); *_target << ltm->tm_min + 1 << ":";
+			_target->width(2); _target->fill('0'); *_target << ltm->tm_sec + 1 << "\t";
+
+			// если мы определяем буффер как список IMessage, то мы не получим доступа к полям DetailedMessage,
+			// и таким образом должны определять формат вывода в рамках подклассов IMessage
+			*_target << PriorityToString(message.getPriority()) << '\t'
+						<< message.getBody() << '\t'
+						<< message.getFile() << '\t'						 
+						<< message.getLine() << std::endl;
+		}
+
+	public:
+
+		static BufferedStreamLogger* Init(std::ostream* target)
+		{
+			static BufferedStreamLogger logger(target);
+			// если хотим инициализировать другим потоком
+			if (target != logger._target)
+			{
+				logger.Flush();	// сбрасываем, всё что накопилось
+				logger._target = target;
+			}
+			return &logger;
+		}
 	};
 
 	// отличается от BufferedStreamLogger тем, что в качестве параметра будет принимать
 	// не поток, а имя файла. Открывать и закрывать файл логгер будет на Flush, чтобы всё
-	// остальное время файл был свободен
-	class BufferedFileLogger : public ILogger
+	// остальное время файл был свободен/
+	// Поведение, при котором файл открывается в конструкторе, а закрывается в деструкторе
+	// оставим потенциальному подклассу BufferedStreamFileLogger : BufferedStreamLogger
+	class BufferedFileLogger : public BufferedStreamLogger
 	{
-		std::list<DetailedMessage> _list;	// IMessage
+	protected:
 		std::string _fileName;
-	public:
-		BufferedFileLogger(std::string fileName) : _fileName(fileName)
+		
+		BufferedFileLogger(std::string fileName) :
+			BufferedStreamLogger(nullptr), _fileName(fileName)
 		{
 			// очищаем файл вывода
 			std::ofstream f;
@@ -223,52 +315,68 @@ namespace TrickyLogger
 			// закрываем файл
 		}
 
-		void Write(DetailedMessage message) override
-		{
-			// разрешаем запись сообщений только с равным или более высоким приоритетом
-			if (message.getPriority() >= this->_priority)
-			{
-				_list.push_back(message);
-			}
-		}
+	public:
 
+		// Гипотетически, можем разрешить публичный конструктор,
+		// чтобы обеспечить ведение логов в разных файлах.
+		// Однако, поскольку Log сейчас всего один, оставляем так
+		static BufferedFileLogger* Init(std::string fileName)
+		{
+			static BufferedFileLogger logger(fileName);
+			
+			// хитрая хитрость - переключаемся на новый объект, если передано другое имя.
+			// Вообще конечно, можем просто запретить изменять цель вывода после инициализации 
+			if (fileName != logger._fileName)
+			{
+				logger.Flush(); // сбрасываем, всё что накопилось
+				logger = BufferedFileLogger(fileName);
+			}
+			return &logger;
+		}
 
 		// "сбрасываем" буфер, записывая его содержимое в целевой поток
 		int Flush() override
 		{
+
 			// открываем файл
 			std::ofstream out;
 			out.open(_fileName, std::ios_base::app);
 
-			for (auto message : _list)
-			{
-				auto curTime = message.getTime();
-				auto ltm = localtime(&curTime);
+			this->_target = &out;
 
-				// вывод времени в "человеческом" виде
-				out << ltm->tm_year + 1900 << "." 
-						 << ltm->tm_mon + 1 << "."
-						 << ltm->tm_mday << " "
-						 << ltm->tm_hour + 1 << ":"
-						 << ltm->tm_min + 1 << ":"
-						 << ltm->tm_sec + 1 << "\t";
-
-				// если мы определяем буффер как список IMessage, то мы не получим доступа к полям DetailedMessage,
-				// и таким образом должны определять формат вывода в рамках подклассов IMessage
-				out << PriorityToString(message.getPriority()) << '\t'
-						 << message.getBody() << '\t'
-						 << message.getFile() << '\t'						 
-						 << message.getLine() << std::endl;
-
-				// кастомизация формата вывода за счёт настроек класса?
-			}
-
-			_list.clear();
+			int result = IBufferedLogger::Flush();
 
 			// закрываем файл
 			out.close();
 
-			return 0;	// возвращать количество записанных сообщений?
+			this->_target = nullptr;
+
+			return result;	// возвращать количество записанных сообщений?
+		}
+	};
+
+	// обёртка вокруг логгера - позволяет обращаться к виртуальным методам как к статичным.
+	// По сути, аналогичного эффекта можно было достичь просто введя глобальную переменную.
+	// Но класс в перспективе позволяет достичь большей гибкости
+	// - например, при желании/необходимости можем запретить изменять назначение лога после инициализации
+	// Терминология. "Лог" - определяет объект, а "логгер" - поведение
+	class Log
+	{
+	private:
+		static ILogger* current;
+
+	public:
+		static void Init(ILogger* logger)
+		{
+			current = logger;
+		}
+
+		static void Write(DetailedMessage message) { if (current) return current->Write(message); }
+		static int Flush() { if (current) return current->Flush(); return 0; }
+		static MessagePriority GetPriority() {return current->GetPriority(); } // возвращает текущий уровень приоритета.
+		static int SetPriority(MessagePriority newPriority) // изменяет уровень приоритета.
+		{ 
+			return current->SetPriority(newPriority);
 		}
 	};
 }
